775c032dfc8206f230ac02751308e28fe64a7afa
Nikhil Ratoliya <nikhilratoliya9@gmail.com>
2025-07-27 01:15:03 +0530

    keycloack issue resolved

--- fitness-app-microservices/aiservice/src/main/java/com/fitness/aiservice/model/Activity.java
+++ fitness-app-microservices/aiservice/src/main/java/com/fitness/aiservice/model/Activity.java
@@ -1,11 +1,17 @@
 package com.fitness.aiservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
     private String id;
     private String userId;
